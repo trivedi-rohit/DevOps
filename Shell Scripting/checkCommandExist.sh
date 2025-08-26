59dc539b7cfddea9b98759327722bf0f60e338de
Rohit Trivedi <126136583+trivedi-rohit@users.noreply.github.com>
2025-08-26 15:31:54 +0530

    checkCommandExist.sh

--- Shell Scripting/checkCommandExist.sh
+++ Shell Scripting/checkCommandExist.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 read -p "Enter a command to check : " cmd_name
 
-if command  -v $cmd_name &> /dev/null ; then
+if command  -v $cmd_name &> /dev/null ; then     # used "/dev/null" to avoid getting output of a command to be printed. So dumped in /dev/null.
 	echo "$cmd_name command exist"
 else
 	echo "$cmd_name doesn't exist"
