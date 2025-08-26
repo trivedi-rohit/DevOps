7f085341a8c68a56ead37bf82221a26ef73311f8
Rohit Trivedi <126136583+trivedi-rohit@users.noreply.github.com>
2025-08-26 19:50:25 +0530

    UpdateBackupScript.sh

--- Shell Scripting/UpdateBackupScript.sh
+++ Shell Scripting/UpdateBackupScript.sh
@@ -9,11 +9,18 @@ if [ -z "$directory_to_backup" ] || [ -z "$backup_location" ]; then
     exit 1
 fi
 
-# Get current date
+# Current date
 current_date=$(date +'%Y-%m-%d')
 
-# Create the backup
+# Backup
 tar -czf "$backup_location/backup-$current_date.tar.gz" -C "$(dirname "$directory_to_backup")" "$(basename "$directory_to_backup")"
 
+status=$?
 # Success message
 echo "Backup of $directory_to_backup is created successfully on $current_date inside $backup_location."
+
+if [ $status -eq 0 ]; then
+	echo "Backup Script executed succesfully"
+else
+	echo "Something went wrong!"
+fi
