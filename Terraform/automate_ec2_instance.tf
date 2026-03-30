626252dd96f2c1118597d5552223d79fb6bc8b51
Rohit Trivedi <126136583+trivedi-rohit@users.noreply.github.com>
2026-03-30 17:47:00 +0530

    Add files via upload

---
+++ Terraform/automate_ec2_instance.tf
@@ -0,0 +1,105 @@
+Create EC2 instance using Terraform : 
+
+# Region 
+
+provider "aws"{ 
+region="us-east-1" 
+
+} 
+
+
+# Key value pair 
+
+resource "aws_key_pair" "my_key_pair" { 
+
+key_name = "terra-automate-key" 
+public_key=file("terra-automate-key.pub") 
+ 
+} 
+
+  
+
+# VPC Default 
+
+resource "aws_default_vpc" "default" { 
+ 
+} 
+
+  
+
+# Security Group 
+
+resource "aws_security_group" "my_security_group" { 
+
+name = "terra-security-group" 
+# interpolatin (use of VPC_id from VPC Default) 
+vpc_id = aws_default_vpc.default.id 
+description = "This  is Inbound & Outbound rules for your instance Security Group"  
+
+} 
+
+  
+
+# Inbound & Outbound port rules: 
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http" { 
+
+security_group_id = aws_security_group.my_security_group.id 
+cidr_ipv4         = "0.0.0.0/0" 
+from_port         = 80 
+ip_protocol       = "tcp" 
+to_port           = 80 
+
+} 
+ 
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" { 
+
+security_group_id = aws_security_group.my_security_group.id 
+cidr_ipv4         = "0.0.0.0/0" 
+from_port         = 22 
+ip_protocol       = "tcp" 
+to_port           = 22 
+
+} 
+
+  
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic" { 
+
+security_group_id = aws_security_group.my_security_group.id 
+cidr_ipv4         = "0.0.0.0/0" 
+ip_protocol       = "-1" # semantically equivalent to all ports 
+
+} 
+
+  
+
+# EC2 instance 
+ 
+resource aws_instance my_instance { 
+
+count = 3                                                  # will create 3 instance ec2 
+ami = "ami-0ec10929233384c7f"        # AMI ID 
+instance_type = "t3.micro" 
+key_name = aws_key_pair.my_key_pair.key_name      # key pair 
+vpc_security_group_ids = [aws_security_group.my_security_group.id]        #vpc & security group 
+
+  
+
+# root storage (EBS) 
+
+root_block_device { 
+
+volume_size = 10 
+volume_type = "gp3"  
+
+} 
+
+  
+tags = { 
+
+Name = "terra-automate-server" 
+
+} 
+
+} 
